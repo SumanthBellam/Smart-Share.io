7e5aaeb289beb4a574991216b1580205f0639ebc
Emily-Kang77 <109045956+Emily-Kang77@users.noreply.github.com>
2024-10-13 15:25:26 -0700

    switch to JS + React, got tailwind to work

--- my-app/src/app/signup-login/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function SignUpLogin() {
-  return (
-    <div>Sign up or login in</div>
-  )
-}
---
+++ smartshare/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,ts,tsx,jsx}", "./src/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
